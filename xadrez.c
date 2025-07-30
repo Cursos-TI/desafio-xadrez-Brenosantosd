e8d6e411c8ffe5fab8450d50deb3ac73bdc5a146
Brenosantosd <brenosantosdaumas@gmail.com>
2025-07-30 23:14:45 +0000

    NOVATO CORRIGIDO

--- xadrez.c
+++ xadrez.c
@@ -6,7 +6,7 @@ int main() {
 // Torre : 5 casas para a direita.
 // bispo : 5 casas para diagonal = para cima e à direita.
 // rainha: 8 casas para a esquerda.
-
+            
 
 // Torre 
 printf("Torre\n");
@@ -17,19 +17,31 @@ for(int t = 0; t < 5; t++) {
 }
 
 // Bispo
+
 printf("Bispo\n");
 
-for(int b = 0; b < 5; b++){
-    printf("Cima esquerda\n");
+int b= 0;
+while(b < 5){
+    printf("Cima, direita\n");
+    b++;
 }
+
+
 
 // Rainha
 printf("Rainha\n");
 
-for(int r = 0; r < 8; r++){
-    printf("Esquerda\n");
+int r = 8;
+do {
+    printf("para esquerda\n");
+    r--;
 
-}
+}while(r > 0);
+
+
+
+
+
 
 
     return 0;
